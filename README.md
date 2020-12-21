dea58e4cedb9b01846ba295f469b6d649dc9027b
William Au <WilliamAuCS@Gmail.com>
2020-12-21 15:14:52 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Created by William Au
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const socket = require("socket.io");
+
+// App setup
+const app = express();
+const PORT = 5050;
+const server = app.listen(PORT, function () {
+  console.log("Listening to requests on " + PORT);
+});
+
+// Socket setup
+const io = socket(server); //What server to work with
+
+io.on("connection", (socket) => {
+  console.log("a user has connected");
+});
