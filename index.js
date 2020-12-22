d3fe7dc48e7070146c201062af78c4de552515ac
William Au <WilliamAuCS@Gmail.com>
2020-12-21 16:13:26 -0800

    Added data to stream

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const server = app.listen(PORT, function () {
 // Socket setup
 const io = socket(server); //What server to work with
 
+function createData() {
+  const num_of_data = 10;
+  const min = 0;
+  const max = 5;
+  var data = [];
+
+  for (let index = 0; index < num_of_data; index++) {
+    let temp = Math.random() * (max - min + 1) + min;
+    data.push(temp);
+  }
+  return data;
+}
+
 io.on("connection", (socket) => {
   console.log("a user has connected");
+
+  let data = createData();
+  io.sockets.emit("graphData", data);
 });
